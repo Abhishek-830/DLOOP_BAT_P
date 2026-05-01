6b8f1d67e19b2936fc6572a8d26b49fde3142520
Abhishek Bhargav <abhishekbhargav8380@gmail.com>
2026-05-01 07:32:51 +0530

    data_ingestion_stage_completed

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from src.cnnClassifier import logger
-import logging
+# from src.cnnClassifier import logger
+# import logging
 
-print(logging.info("Welcome to Ml Batch p logs"))
+# print(logging.info("Welcome to Ml Batch p logs"))
+
+
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
